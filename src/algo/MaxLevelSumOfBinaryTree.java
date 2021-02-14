f95a7024810f82cd886e0a3c4a05c60f0c3f4488
Jim Wen <jimwen@lyft.com>
2021-02-13 16:17:09 -0800

    level sum max

---
+++ src/algo/MaxLevelSumOfBinaryTree.java
@@ -0,0 +1,42 @@
+package algo;
+
+import datastructures.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxLevelSumOfBinaryTree {
+    public int maxLevelSum(TreeNode root) {
+        Map<Integer, Integer> levelToSum = new HashMap<>();
+        traverse(levelToSum, root, 1);
+        return getMaxLevel(levelToSum);
+    }
+
+    private void traverse(Map<Integer, Integer> levelToSum, TreeNode root, int i) {
+        if (root == null) {
+            return;
+        }
+
+        int levelSum = levelToSum.getOrDefault(i, 0);
+        levelToSum.put(i, levelSum + root.val);
+
+        traverse(levelToSum, root.left, i+1);
+        traverse(levelToSum, root.right, i+1);
+    }
+
+    private int getMaxLevel(Map<Integer, Integer> levelToValues) {
+        int maxLevel = -1;
+        int maxLevelSum = -1;
+
+        for (Map.Entry<Integer, Integer> entry : levelToValues.entrySet()) {
+            int level = entry.getKey();
+            int sum = entry.getValue();
+
+            if (sum > maxLevelSum) {
+                maxLevelSum = sum;
+                maxLevel = level;
+            }
+        }
+        return maxLevel;
+    }
+}

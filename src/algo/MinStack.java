7c2a58f40b1384a94ea8c04c847087d3dcb74781
Jim Wen <jimwen@lyft.com>
2021-02-26 12:36:20 -0800

    minstack

---
+++ src/algo/MinStack.java
@@ -0,0 +1,40 @@
+package algo;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+
+    Deque<Integer> stack = new LinkedList<>();
+    Deque<Integer> minStack = new LinkedList<>();
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+
+}
